88d58a2ae803c6fae12a3b81ee2535dde9802d74
lizexin <zexin-li@outlook.com>
2023-12-11 20:14:55 +0800

    oauth2 jwt - resource server ok

---
+++ spring-security-oauth2-jwt/security-oauth2-resource-server-jwt/src/test/java/io/github/blankbro/securityoauth2resourceserverjwt/SecurityOauth2ResourceServerJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.blankbro.securityoauth2resourceserverjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOauth2ResourceServerJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
