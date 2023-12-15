5b761bb5364a4c2a5955c56e96091afdaa7410bd
lizexin <zexin-li@outlook.com>
2023-12-15 20:27:11 +0800

    tokenenhancer 初始化 成功了终于

--- spring-security-oauth2/authorization-server-tokenenhancer/src/main/java/io/github/blankbro/authorizationservertokenenhancer/config/CustomTokenEnhancer.java
+++ spring-security-oauth2/authorization-server-tokenenhancer/src/main/java/io/github/blankbro/authorizationservertokenenhancer/config/CustomTokenEnhancer.java
@@ -1,28 +1,28 @@
-// package io.github.blankbro.authorizationservertokenenhancer.config;
-//
-// import lombok.extern.slf4j.Slf4j;
-// import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
-// import org.springframework.security.oauth2.common.OAuth2AccessToken;
-// import org.springframework.security.oauth2.provider.OAuth2Authentication;
-// import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-// import org.springframework.stereotype.Component;
-//
-// import java.util.Map;
-//
-// @Slf4j
-// @Component
-// public class CustomTokenEnhancer implements TokenEnhancer {
-//
-//     @Override
-//     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
-//         OAuth2AccessToken enhanceOAuth2AccessToken = accessToken;
-//         log.info("CustomTokenEnhancer...");
-//         if (accessToken instanceof DefaultOAuth2AccessToken) {
-//             enhanceOAuth2AccessToken = new DefaultOAuth2AccessToken(accessToken);
-//             Map<String, Object> additionalInformation = enhanceOAuth2AccessToken.getAdditionalInformation();
-//             additionalInformation.put("custom-enhance", "oh yeah");
-//         }
-//         return enhanceOAuth2AccessToken;
-//     }
-//
-// }
+package io.github.blankbro.authorizationservertokenenhancer.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Slf4j
+@Component
+public class CustomTokenEnhancer implements TokenEnhancer {
+
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+        OAuth2AccessToken enhanceOAuth2AccessToken = accessToken;
+        log.info("CustomTokenEnhancer...");
+        if (accessToken instanceof DefaultOAuth2AccessToken) {
+            enhanceOAuth2AccessToken = new DefaultOAuth2AccessToken(accessToken);
+            Map<String, Object> additionalInformation = enhanceOAuth2AccessToken.getAdditionalInformation();
+            additionalInformation.put("custom-enhance", "oh yeah");
+        }
+        return enhanceOAuth2AccessToken;
+    }
+
+}
