e997b31cdb16b500bb8effa36bbae43406185535
lizexin <zexin-li@outlook.com>
2023-12-20 17:58:43 +0800

    wechat social 搞不定了呀

--- spring-security-oauth2/authorization-server-social/src/main/java/io/github/blankbro/authorizationserversocial/social/wechat/config/WeChatSocialConfiguration.java
+++ spring-security-oauth2/authorization-server-social/src/main/java/io/github/blankbro/authorizationserversocial/social/wechat/config/WeChatSocialConfiguration.java
@@ -2,9 +2,13 @@ package io.github.blankbro.authorizationserversocial.social.wechat.config;
 
 import io.github.blankbro.authorizationserversocial.social.wechat.connect.WeChatOAuth2ConnectionFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.social.UserIdSource;
 import org.springframework.social.autoconfigure.SocialAutoConfigurerAdapter;
+import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.connect.ConnectionFactory;
+import org.springframework.social.security.AuthenticationNameUserIdSource;
 
+@EnableSocial
 @Configuration
 public class WeChatSocialConfiguration extends SocialAutoConfigurerAdapter {
 
@@ -15,5 +19,10 @@ public class WeChatSocialConfiguration extends SocialAutoConfigurerAdapter {
                 "123",
                 "123"
         );
+    }
+
+    @Override
+    public UserIdSource getUserIdSource() {
+        return new AuthenticationNameUserIdSource();
     }
 }
